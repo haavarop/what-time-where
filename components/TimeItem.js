8c7f74182ac1cfa8c11ad0d14305529407bfeaf2
Håvard Opheim <haavard@vaage.com>
2019-08-23 19:31:37 +0200

    Add optional render for special timezones

--- components/TimeItem.js
+++ components/TimeItem.js
@@ -6,14 +6,13 @@ const TimeItem = props => {
   const {color, utc_format, places, tz_name1st, name} = props;
   
   const calculateLocalDate = (local_utc) => {    
-    const t = moment().tz(local_utc)
-    return t ? t.format("HH:mm:ss") : local_utc;
+    return moment().tz(local_utc).format("HH:mm:ss")
   }
 
   return (
     <div style={{backgroundColor: color}} className="item-container">
-      <h2 className="text">{tz_name1st}</h2>
-      <h4 className="text">{calculateLocalDate(tz_name1st)}</h4>
+      <h2 className="text">{tz_name1st ? tz_name1st : places}</h2>
+      <h4 className="text">{tz_name1st ? calculateLocalDate(tz_name1st) : name}</h4>
     </div>
   );
 };
