8813f9d5250cdfdcb377b378b93af1bbcfaea3d4
Håvard Opheim <haavard@vaage.com>
2019-08-23 19:30:22 +0200

    Add title

---
+++ components/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Title.css";
+
+const Title = () => {
+  return (
+    <>
+      <h1 className="text">What time where!</h1>
+      <h3 className="text">What time is where in the world?</h3>    
+    </>
+  );
+}
+
+export default Title;
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import TimeList from "../components/TimeList";
 import Map from "../components/Map";
 import "../styles/global.css";
+import Title from "../components/Title";  
 
 const Home = () => {
   const [timezonesToShow, setTimezonesToShow] = useState([])
   return (
     <div>
+      <Title />
       <TimeList timezones={timezonesToShow}/>
-      <Map onTimeZoneClick={setTimezonesToShow}/>
+      <div className="map-container">
+        <Map onTimeZoneClick={setTimezonesToShow}/>
+      </div>
     </div>
   )
 }
