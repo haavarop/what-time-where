2d65f55ff8d492038f6caa021003cb08efad8f1d
Håvard Opheim <haavard@vaage.com>
2019-08-23 18:55:29 +0200

    Add global stylesheet and font

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import TimeList from "../components/TimeList";
 import Map from "../components/Map";
+import "../styles/global.css";
 
 const Home = () => {
   const [timezonesToShow, setTimezonesToShow] = useState([])
