391b82cbb2db8776f25d0c926f4051b1ad50979e
Håvard Opheim <haavard@vaage.com>
2019-08-28 10:19:45 +0200

    Add favicon and title

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 import TimeList from "../components/TimeList";
 import Map from "../components/Map";
 import "../styles/global.css";
-import Title from "../components/Title";  
+import Title from "../components/Title"; 
+import Head from "next/head" 
 
 const Home = () => {
   const [timezonesToShow, setTimezonesToShow] = useState([])
   return (
     <div>
+      <Head>
+        <title>What time where</title>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
       <Title />
       <TimeList timezones={timezonesToShow}/>
       <div className="map-container">
